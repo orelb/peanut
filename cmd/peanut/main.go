7931e00a659cac7ea72a534e26dc87b10ac74fd0
Orel Biton <orelbiton27@gmail.com>
2020-04-16 10:58:37 +0300

    Added CLI functionality

--- cmd/peanut/main.go
+++ cmd/peanut/main.go
@@ -2,13 +2,41 @@ package main
 
 import (
 	"github.com/orelb/peanut/pkg/peanut"
+	"github.com/urfave/cli/v2"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
 func main() {
-	configContents, err := ioutil.ReadFile("peanut.yml")
+	var configFile string
+
+	app := &cli.App{
+		Name:  "Peanut",
+		Usage: "fetch files from different sources to help compose your static website.",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "config-file",
+				Aliases:     []string{"c"},
+				Value:       "peanut.yml",
+				Usage:       "path to peanut config file",
+				Destination: &configFile,
+			},
+		},
+		Action: func(context *cli.Context) error {
+			pullAllSources(configFile)
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func pullAllSources(configFile string) {
+	configContents, err := ioutil.ReadFile(configFile)
 	if err != nil {
 		log.Fatalf("Failed to load config file: %s", err)
 	}
